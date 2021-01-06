54061ee84df67c68ae008f4211560d0f0d21dd89
suleyman <suleyman.sevimli1@gmail.com>
2021-01-07 00:35:58 +0300

    init project & storybook structure created & css color variable created

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta charSet={"UTF-8"}/>
       </Head>
 
       <main className={styles.main}>
