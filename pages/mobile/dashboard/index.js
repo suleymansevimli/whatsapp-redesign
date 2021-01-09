e938ab607368192821d0a3afb0b7371bac08f244
suleyman <suleyman.sevimli1@gmail.com>
2021-01-09 17:56:39 +0300

    swipe actions added to userInformation component

--- pages/mobile/dashboard/index.js
+++ pages/mobile/dashboard/index.js
@@ -2,6 +2,12 @@ import React, {useState} from "react";
 import style from '../../../styles/mobile/dashboard/index.module.css'
 import MobileLayout from "../../../components/Layout/MobileLayout";
 import UserInformation from "../../../components/UserInformation/UserInformation";
+import {
+    SwipeableList,
+    SwipeableListItem
+} from '@sandstreamdev/react-swipeable-list';
+import '@sandstreamdev/react-swipeable-list/dist/styles.css';
+import Gestures from "../../../components/Gestures/Gestures";
 
 const Dashboard = () => {
 
@@ -13,15 +19,67 @@ const Dashboard = () => {
         {avatar: "/img/kennedy.jpg", name: "Rahmetli Başkan Kennedy"},
     ]
 
+    const [swipeProgress, handleSwipeProgress] = useState();
+    const [swipeAction, handleSwipeAction] = useState('None');
+
+    const swipeLeftOptions = () => ({
+        content: <Gestures/>,
+        action: () => handleSwipeProgress(swipeProgress)
+    });
+
+    const handleSwipeStart = () => {
+        // console.log('Swipe started');
+    };
+
+    const handleSwipeEnd = () => {
+        // console.log('Swipe ended');
+    };
 
     return (
         <MobileLayout header headerTitle={"Messages"} tabBar>
             <div className={style.container}>
-                <UserInformation type={"received"} user={users[0]} unreadMessageCount={1} lastMessageTime={"13:45"} lastMessage={"Hello :)"}/>
-                <UserInformation type={"sent"} user={users[1]} lastMessageTime={"13:45"} status={"read"} lastMessage={"Halı saha maçı kaçtaydı ?"}/>
-                <UserInformation type={"received"} user={users[2]} unreadMessageCount={1} lastMessageTime={"13:45"} lastMessage={"Hi :) What are you doing now?"}/>
-                <UserInformation type={"received"} user={users[3]} unreadMessageCount={1} lastMessageTime={"13:45"} lastMessage={"Benim ayağım sakat gelemiyorum."}/>
-                <UserInformation type={"sent"} status={"pending"} user={users[4]} lastMessageTime={"13:45"} lastMessage={"Başkanım önemli bir durum var !"}/>
+                <SwipeableList>
+                    <SwipeableListItem
+                        swipeLeft={swipeLeftOptions()}
+                        onSwipeEnd={handleSwipeEnd}
+                        onSwipeProgress={handleSwipeProgress}
+                        onSwipeStart={handleSwipeStart}>
+                        <UserInformation type={"received"} user={users[0]} unreadMessageCount={1} lastMessageTime={"13:45"} lastMessage={"Hello :)"}/>
+                    </SwipeableListItem>
+
+                    <SwipeableListItem
+                        swipeLeft={swipeLeftOptions()}
+                        onSwipeEnd={handleSwipeEnd}
+                        onSwipeProgress={handleSwipeProgress}
+                        onSwipeStart={handleSwipeStart}>
+                        <UserInformation type={"sent"} user={users[1]} lastMessageTime={"13:45"} status={"read"} lastMessage={"Halı saha maçı kaçtaydı ?"}/>
+                    </SwipeableListItem>
+
+                    <SwipeableListItem
+                        swipeLeft={swipeLeftOptions()}
+                        onSwipeEnd={handleSwipeEnd}
+                        onSwipeProgress={handleSwipeProgress}
+                        onSwipeStart={handleSwipeStart}>
+                        <UserInformation type={"received"} user={users[2]} unreadMessageCount={1} lastMessageTime={"13:45"} lastMessage={"Hi :) What are you doing now?"}/>
+                    </SwipeableListItem>
+
+                    <SwipeableListItem
+                        swipeLeft={swipeLeftOptions()}
+                        onSwipeEnd={handleSwipeEnd}
+                        onSwipeProgress={handleSwipeProgress}
+                        onSwipeStart={handleSwipeStart}>
+                        <UserInformation type={"received"} user={users[3]} unreadMessageCount={1} lastMessageTime={"13:45"} lastMessage={"Benim ayağım sakat gelemiyorum."}/>
+                    </SwipeableListItem>
+
+
+                    <SwipeableListItem
+                        swipeLeft={swipeLeftOptions()}
+                        onSwipeEnd={handleSwipeEnd}
+                        onSwipeProgress={handleSwipeProgress}
+                        onSwipeStart={handleSwipeStart}>
+                        <UserInformation type={"sent"} status={"pending"} user={users[4]} lastMessageTime={"13:45"} lastMessage={"Başkanım önemli bir durum var !"}/>
+                    </SwipeableListItem>
+                </SwipeableList>
             </div>
         </MobileLayout>
     )
